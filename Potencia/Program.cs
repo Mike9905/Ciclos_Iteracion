02906e12dae079ba90b6508f11f93a8ee3bc4694
mikew <miguel.galvan@uabc.edu.mx>
2022-04-07 21:31:48 -0700

    Add project files.

---
+++ Potencia/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Potencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Dime un numero");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Dime otro numero");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            double potencia = 1;
+
+            int exponente = num2;
+            if (num2 < 0) exponente = -num2;
+
+            int i = 1;
+            while (i <= exponente)
+            {
+                potencia *= num1;
+                i++;
+            }
+            if (num2 < 0) potencia = 1 / potencia;
+            Console.WriteLine(potencia);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Primos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, j, i;
+
+            for (j = 2; j <= 100; j++)
+            { //por definicion el 1 no es primo, se empieza en 2
+                a = 0;
+                for (i = 1; i <= 100; i++)//divide a j entre los números del 1 al 100
+                {
+                    if (j % i == 0) 
+                        a++;
+                }
+                if (a == 2)
+                { 
+
+                    Console.WriteLine("");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
